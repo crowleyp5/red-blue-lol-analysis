784de1d7670b29139843d403c4ee6807095995b4
Paul Crowley <108026566+crowleyp5@users.noreply.github.com>
2023-12-13 23:26:19 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
 
+# Load your data
 lol = pd.read_csv('loldata_cleaned.csv')
 
 st.title('League of Legends Data Exploration')
@@ -37,13 +38,8 @@ if selected_red_bot != 'Any':
 
 # Function to create and display a box plot
 def display_boxplot(data, title, role):
-    column_name = f'{role} CS Diff'
-    if column_name not in data.columns or data.empty:
-        st.write(f"No data available for {title}.")
-        return
-
     plt.figure(figsize=(10, 6))
-    sns.boxplot(data=data, x=column_name)
+    sns.boxplot(data=data, x=f'{role} CS Diff')
     plt.title(title)
     st.pyplot(plt)
 
@@ -63,14 +59,11 @@ selected_blue_jungler = st.sidebar.selectbox('Select Blue Side Jungler', ['Any']
 # Sidebar for jungler selection - Red Side
 selected_red_jungler = st.sidebar.selectbox('Select Red Side Jungler', ['Any'] + list(lol['Red Jungle'].unique()))
 
-# Initialize filtered DataFrame
-jungle_df = lol
-
 # Apply filters dynamically based on user selection
 if selected_blue_jungler != 'Any':
-    jungle_df = jungle_df[jungle_df['Blue Jungle'] == selected_blue_jungler]
+    filtered_df = filtered_df[filtered_df['Blue Jungle'] == selected_blue_jungler]
 if selected_red_jungler != 'Any':
-    jungle_df = jungle_df[jungle_df['Red Jungle'] == selected_red_jungler]
+    filtered_df = filtered_df[filtered_df['Red Jungle'] == selected_red_jungler]
 
 # Function to create and display a bar chart for the number of dragons
 def display_bar_chart(data, title):
@@ -84,7 +77,7 @@ def display_bar_chart(data, title):
 # Preparing data for the bar chart
 dragon_data = pd.DataFrame({
     'Team': ['Blue', 'Red'],
-    'Dragons': [jungle_df['Blue Dragons'].sum(), jungle_df['Red Dragons'].sum()]
+    'Dragons': [filtered_df['Blue Dragons'].sum(), filtered_df['Red Dragons'].sum()]
 })
 
 # Display bar chart for the number of dragons
@@ -92,51 +85,21 @@ st.header('Dragon Control Analysis')
 display_bar_chart(dragon_data, 'Total Number of Dragons Secured by Each Team')
 
 # Dropdown for X-Axis Selection
-x_axis_variable = st.selectbox(
-    'Select X-Axis Variable',
-    ['First Dragon Time', 'First Rift Herald Time']
-)
-
-# Team filter based on the selected objective
-team_filter = st.selectbox(
-    'Select Team that Secured the Objective',
-    ['Any', 'Blue', 'Red']
-)
-
-# Filter data based on team selection
-if team_filter != 'Any':
-    if x_axis_variable == 'First Dragon Time':
-        time_df = lol[lol['First Dragon Team'] == team_filter]
-    else:
-        time_df = lol[lol['First Rift Herald Team'] == team_filter]
-else:
-    time_df = lol
+x_axis_variable = st.selectbox('Select X-Axis Variable', ['First Dragon Time', 'First Rift Herald Time'])
 
 # Function to create and display a scatter plot
 def display_scatter_plot(data, x_variable, y_variable):
-    # Directly use the column names as they appear in your DataFrame
-    if x_variable == 'First Dragon Time':
-        x_col = 'First Dragon Time'  # Replace with the exact column name from your DataFrame
-    elif x_variable == 'First Rift Herald Time':
-        x_col = 'First Rift Herald Time'  # Replace with the exact column name from your DataFrame
-
-    y_col = 'Game Time'  # Replace with the exact game time column name from your DataFrame
-
-    if data.empty or x_col not in data.columns or y_col not in data.columns:
-        st.error(f"No data available or incorrect column names for plotting {x_col} vs {y_col}.")
+    if data.empty or x_variable not in data.columns or y_variable not in data.columns:
+        st.error(f"No data available or incorrect column names for plotting {x_variable} vs {y_variable}.")
         return
 
     plt.figure(figsize=(10, 6))
-    sns.scatterplot(x=x_col, y=y_col, data=data)
-    plt.title(f'{x_col} vs {y_col}')
-    plt.xlabel(x_col)
-    plt.ylabel(y_col)
+    sns.scatterplot(x=x_variable, y=y_variable, data=data)
+    plt.title(f'{x_variable} vs {y_variable}')
+    plt.xlabel(x_variable)
+    plt.ylabel(y_variable)
     st.pyplot(plt)
 
 # Display scatter plot
 st.header('Objective Timing vs Game Time')
-display_scatter_plot(
-    filtered_df,
-    x_axis_variable.replace(' ', ''),  # Adjust column name if necessary
-    'GameTime'  # Adjust column name for game time if necessary
-)
+display_scatter_plot(filtered_df, x_axis_variable, 'Game Time')
